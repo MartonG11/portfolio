c58085e3dab4dc6544f0717238b25a5186972658
Gábor Márton <32680835+MartonG11@users.noreply.github.com>
2020-10-28 13:48:39 +0100

    local project uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,47 @@
+const navMenu = document.getElementById("nav-menu"),
+  toggleMenu = document.getElementById("nav-toggle"),
+  closeMenu = document.getElementById("nav-close");
+
+// SHOW MENU
+toggleMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("show");
+});
+
+// HIDE MENU
+closeMenu.addEventListener("click", () => {
+  navMenu.classList.remove("show");
+});
+
+// HIDE MENU ON ITEMCLICK
+const navLink = document.querySelectorAll(".nav__link");
+
+function linkAction() {
+  navMenu.classList.remove("show");
+}
+navLink.forEach((n) => n.addEventListener("click", linkAction));
+
+// SCROLL TO THE CLICKED NAVLINK
+
+const sections = document.querySelectorAll("section[id]");
+
+window.addEventListener("scroll", scrollActive);
+
+function scrollActive() {
+  const scrollY = window.pageYOffset;
+
+  sections.forEach((current) => {
+    const sectionHeight = current.offsetHeight;
+    const sectionTop = current.offsetTop - 50;
+    sectionId = current.getAttribute("id");
+
+    if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.add("active");
+    } else {
+      document
+        .querySelector(".nav__menu a[href*=" + sectionId + "]")
+        .classList.remove("active");
+    }
+  });
+}
