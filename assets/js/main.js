2188b82ab5164d87831e53b29704101c85a7d9f3
Gabor Marton <gabormarton91@gmail.com>
2020-10-28 18:10:39 +0100

    feature: theme select added

--- assets/js/main.js
+++ assets/js/main.js
@@ -45,3 +45,39 @@ function scrollActive() {
     }
   });
 }
+
+// CHANGE THEME
+
+let theme = localStorage.getItem('theme')
+
+if(theme == null){
+  setTheme('light')
+}else{
+  setTheme(theme)
+}
+
+let themeDots = document.getElementsByClassName('theme-dot');
+
+for (var i=0; themeDots.length > i; i++){
+  themeDots[i].addEventListener('click', function(){
+    let mode = this.dataset.mode
+    setTheme(mode)
+  })
+}
+
+function setTheme(mode){
+  if(mode == 'default'){
+    document.getElementById('theme-style').href = 'assets/css/default.css'
+  }
+  if(mode == 'red'){
+    document.getElementById('theme-style').href = 'assets/css/red.css'
+  }
+  if(mode == 'green'){
+    document.getElementById('theme-style').href = 'assets/css/green.css'
+  }
+  if(mode == 'grey'){
+    document.getElementById('theme-style').href = 'assets/css/grey.css'
+  }
+
+  localStorage.setItem('theme', mode);
+}
